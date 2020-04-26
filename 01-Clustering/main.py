3c9320c237f836e429745311b82d75b2c3ba89fa
Pols97 <jeanpols97@gmail.com>
2020-04-26 22:48:14 +0200

    lastclusteringfinalfinal

--- 01-Clustering/main.py
+++ 01-Clustering/main.py
@@ -3,13 +3,12 @@ from clusterinfosets import *
 
 # Choose game:
 
-#game = "kuhn"
-#game = "leduc3"
-game = "leduc3"
+game = "leduc5"
 
 # Loads the csv and gets the proper infoset
 infosets, rawinfosets = loadinfosets(game)
 
+print(infosets)
 # Merges infosets without loss of information
 infosets = cluster(infosets, infoloss = False)
 
@@ -20,4 +19,9 @@ print(infosets)
 infosets = cluster(infosets, infoloss = True)
 
 print(infosets)
+
+# Collects the initial data left behind
+infosets = infosetstoprint(infosets,rawinfosets)
+
+# Saves it
 infosets.to_csv("..\\Import-files\\clust_"+game+".csv", index = False, header = True)
